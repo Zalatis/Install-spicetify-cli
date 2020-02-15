b9385b517faeb20e3d392b4be389f1c541e7cf35
Zalati <y4su.ced@gmail.com>
2020-02-15 02:10:45 +0100

    Update Install Spicetify FR.bat

--- Install Spicetify FR.bat
+++ Install Spicetify FR.bat
@@ -1,40 +1,156 @@
 @echo off
 Title Install Spicetify, Script made by Zalati
-:start
+:menu
+echo --------------------
+echo -- Menu du Script --
+echo --------------------
+echo.
+echo 1 - Installer Spicetify
+echo 2 - Installer/Update les themes
+echo 3 - Re-Appliquer ou changer le theme
+echo 4 - Remettre le theme de base de Spotify
+echo 5 - Activer/Desactiver le devtool
+echo 6 - Quitter
+set /p reponse1="Que voulez vous faire ? (Repondez avec le numero) : "
+If /i "%reponse1%"=="1" GOTO :InstallSpicetify
+If /i "%reponse1%"=="2" GOTO :DownloadThemes
+If /i "%reponse1%"=="3" GOTO :ApplyTheme
+If /i "%reponse1%"=="4" GOTO :RestoreSpotify
+If /i "%reponse1%"=="5" GOTO :DevTool
+If /i "%reponse1%"=="6" GOTO :end
+ECHO "%reponse1%" n'est pas valide !
+GOTO :error1
+
+:InstallSpicetify
+cls
 %SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass "Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/khanhas/spicetify-cli/master/install.ps1" | Invoke-Expression"
 spicetify
-%SystemRoot%\explorer.exe "%HOMEPATH%\spicetify-cli\Themes\SpicetifyDefault"
-SET /p choice=Voulez-vous installer quelques themes? Oui ou Non, (par défaut: Oui): 
-IF NOT '%choice%'=='' SET choice=%choice:~0,1%
-IF '%choice%'=='O' GOTO oui
-IF '%choice%'=='Oui' GOTO oui
-IF '%choice%'=='Y' GOTO oui
-IF '%choice%'=='y' GOTO oui
-IF '%choice%'=='yes' GOTO oui
-IF '%choice%'=='Yes' GOTO oui
-IF '%choice%'=='N' GOTO non
-IF '%choice%'=='n' GOTO non
-IF '%choice%'=='no' GOTO non
-IF '%choice%'=='non' GOTO non
-ECHO "%choice%" n'est pas valide !
-ECHO.
+cls
+GOTO InstallSpicetifyStep2
+:InstallSpicetifyStep2
+echo Que voulez-vous faire ? (Repondez avec le numero) : 
+echo 1 - Installer des themes
+echo 2 - Ne pas installer de themes
+SET /p reponse2=""
+If /i "%reponse2%"=="" GOTO DownloadThemes
+If /i "%reponse2%"=="1" GOTO DownloadThemes
+If /i "%reponse2%"=="2" GOTO NoDownloadThemes
+ECHO "%reponse2%" n'est pas valide !
+GOTO :error2
 
-:non
+::Telecharge les themes
+:DownloadThemes
 cls
-echo Faites les modifications de couleurs dans color.ini et les modifications du css dans user.css
-set /p=Puis appuyez sur la touche Enter pour continuer...
+%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -command "Start-BitsTransfer -Source https://github.com/morpheusthewhite/spicetify-themes/archive/master.zip -Destination C:\%HOMEPATH%\.spicetify\themes.zip"
+%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -command "Expand-Archive C:\%HOMEPATH%\.spicetify\themes.zip C:\%HOMEPATH%\.spicetify\Themes"
+ECHO Veuillez patienter pendant le telechargement des themes.
+robocopy C:\%HOMEPATH%\.spicetify\Themes\spicetify-themes-master C:\%HOMEPATH%\.spicetify\Themes\ *.* /E /XD %HOMEPATH\.spicetify\Themes\ /move
+del /Q /S "C:\%HOMEPATH%\.spicetify\themes.zip" "C:\%HOMEPATH%\.spicetify\Themes\.gitignore" "C:\%HOMEPATH%\.spicetify\Themes\CODE_OF_CONDUCT.md" "C:\%HOMEPATH%\.spicetify\Themes\LICENSE" "C:\%HOMEPATH%\.spicetify\Themes\README.md"
+@RD /S /Q C:\%HOMEPATH%\.spicetify\Themes\spicetify-themes-master
+start https://github.com/morpheusthewhite/spicetify-themes
+cls
+SET /p whichtheme="Voici tous les themes disponibles, choissisez-en un, et entrez son nom (La casse est prise en compte) : "
+IF '%whichtheme%'=='' SET whichtheme = 
+IF '%whichtheme%'=='%whichtheme%' SET whichtheme = %whichtheme%
+SET whichtheme=%whichtheme%
+spicetify config current_theme %whichtheme%
+spicetify backup
+spicetify apply
 spicetify update
-spicetify backup apply enable-devtool
+SET /p=Appuyez sur la touche Enter pour retourner au menu...
+cls
+GOTO menu
 
-:oui
+::Ne telecharge pas les themes
+:NoDownloadThemes
 cls
-%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -command "Start-BitsTransfer -Source https://github.com/morpheusthewhite/spicetify-themes/archive/master.zip -Destination %HOMEPATH%\.spicetify\themes.zip"
-%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -command "Expand-Archive %HOMEPATH%\.spicetify\themes.zip %HOMEPATH%\.spicetify\Themes"
-robocopy %HOMEPATH%\.spicetify\Themes\spicetify-themes-master %HOMEPATH%\.spicetify\Themes\ *;* /E /XD %HOMEPATH\.spicetify\Themes\ /move
-del /Q /S "%HOMEPATH%\.spicetify\themes.zip" "%HOMEPATH%\.spicetify\Themes\.gitignore" "%HOMEPATH%\.spicetify\Themes\CODE_OF_CONDUCT.md" "%HOMEPATH%\.spicetify\Themes\LICENSE" "%HOMEPATH%\.spicetify\Themes\README.md"
-cls
+%SystemRoot%\explorer.exe "C:\%HOMEPATH%\spicetify-cli\Themes\SpicetifyDefault"
 echo Faites les modifications de couleurs dans color.ini et les modifications du css dans user.css
-set /p=Puis appuyez sur la touche Enter pour continuer...
+SET /p=Puis appuyez sur la touche Enter pour continuer...
+spicetify backup
+spicetify apply
 spicetify update
-spicetify backup apply enable-devtool
+cls
+SET /p=Appuyez sur la touche Enter pour retourner au menu...
+cls
+GOTO menu
+
+::Applique le theme choisi
+:ApplyTheme
+cls
+start https://github.com/morpheusthewhite/spicetify-themes
+cls
+SET /p whichtheme="Voici tous les themes disponibles, choissisez-en un, et entrez son nom (La casse est prise en compte), pour appliquer le theme actuel vous devez juste appuyez sur la touche Enter : "
+IF '%whichtheme%'=='' SET whichtheme = 
+IF '%whichtheme%'=='%whichtheme%' SET whichtheme = %whichtheme%
+SET whichtheme=%whichtheme%
+spicetify config current_theme %whichtheme%
+cls
+spicetify backup
+spicetify apply
+cls
+SET /p=Appuyez sur la touche Enter pour retourner au menu...
+cls
+GOTO menu
+
+::Restaure le theme de base de Spotify
+:RestoreSpotify
+cls
+spicetify restore backup
+cls
+SET /p=Appuyez sur la touche Enter pour retourner au menu...
+cls
+GOTO menu
+
+::Active ou Desactive le devtool
+:DevTool
+cls
+echo Que voulez-vous faire ? (Repondez avec le numero) : 
+echo 1 - Activer le DevTool
+echo 2 - Desactiver le DevTool
+SET /p reponse3=""
+If /i "%reponse3%"=="" GOTO EnableDevTool
+If /i "%reponse3%"=="1" GOTO EnableDevTool
+If /i "%reponse3%"=="2" GOTO DisableDevTool
+ECHO "%reponse3%" n'est pas valide !
+GOTO :error3
+
+::Active le DevTool
+:EnableDevTool
+cls
+spicetify enable-devtool
+cls
+SET /p=Appuyez sur la touche Enter pour retourner au menu...
+cls
+GOTO menu
+
+::Desactive le DevTool
+:DisableDevTool
+cls
+spicetify disable-devtool
+cls
+SET /p=Appuyez sur la touche Enter pour retourner au menu...
+cls
+GOTO menu
+
+::Erreur
+:error1
+SET /p=Appuyez sur la touche Enter pour retourner au menu...
+cls
+GOTO menu
+
+::Erreur
+:error2
+SET /p=Appuyez sur la touche Enter pour retourner au menu...
+cls
+GOTO InstallSpicetifyStep2
+
+::Erreur
+:error3
+SET /p=Appuyez sur la touche Enter pour retourner au menu...
+cls
+GOTO DevTool
+
+::Ferme le script
+:end
 exit
