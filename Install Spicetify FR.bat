2573fc8114ad1174dbe6eaddec9a28389502824a
Zalati <y4su.ced@gmail.com>
2020-02-26 05:23:35 +0100

    Update Install Spicetify FR.bat

--- Install Spicetify FR.bat
+++ Install Spicetify FR.bat
@@ -1,36 +1,62 @@
 @echo off
 Title Install Spicetify, Script made by Zalati
 :menu
+cls
 echo --------------------
 echo -- Menu du Script --
 echo --------------------
 echo.
-echo 1 - Installer Spicetify
-echo 2 - Installer/Update les themes
-echo 3 - Re-Appliquer ou changer le theme
-echo 4 - Remettre le theme de base de Spotify
-echo 5 - Activer/Desactiver le devtool
-echo 6 - Quitter
-set /p reponse1="Que voulez vous faire ? (Repondez avec le numero) : "
-If /i "%reponse1%"=="1" GOTO :InstallSpicetify
-If /i "%reponse1%"=="2" GOTO :DownloadThemes
-If /i "%reponse1%"=="3" GOTO :ApplyTheme
-If /i "%reponse1%"=="4" GOTO :RestoreSpotify
-If /i "%reponse1%"=="5" GOTO :DevTool
-If /i "%reponse1%"=="6" GOTO :end
+echo Que voulez-vous faire ? (Repondes avec le numero)
+echo 1 - Installer Spotify
+echo 2 - Installer Spicetify
+echo 3 - Installer/Update les themes
+echo 4 - Re-Appliquer ou changer le theme
+echo 5 - Remettre le theme de base de Spotify
+echo 6 - Activer/Desactiver le devtool
+echo 7 - Bloquer/Debloquer les pubs (version test)
+echo 8 - Quitter
+echo.
+SET /p reponse1="Votre choix : "
+If /i "%reponse1%"=="1" GOTO :InstallSpotify
+If /i "%reponse1%"=="2" GOTO :InstallSpicetify
+If /i "%reponse1%"=="3" GOTO :DownloadThemes
+If /i "%reponse1%"=="4" GOTO :ApplyTheme
+If /i "%reponse1%"=="5" GOTO :RestoreSpotify
+If /i "%reponse1%"=="6" GOTO :DevTool
+If /i "%reponse1%"=="7" GOTO :DisableAdsMenu
+If /i "%reponse1%"=="8" GOTO :end
 ECHO "%reponse1%" n'est pas valide !
 GOTO :error1
 
+:: Installe Spotify
+:InstallSpotify
+cls
+%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -command "Start-BitsTransfer -Source https://download.scdn.co/SpotifySetup.exe -Destination C:\%HOMEPATH%\Downloads\SpotifySetup.exe"
+cls
+C:\%HOMEPATH%\Downloads\SpotifySetup.exe /Silent
+cls
+del /Q /S C:\%HOMEPATH%\Downloads\SpotifySetup.exe
+cls
+echo Appuyez sur une touche pour retourner au menu.
+pause > nul
+cls
+GOTO menu
+
+:: Installe Spicetify
 :InstallSpicetify
+cls
+%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass "Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/khanhas/spicetify-cli/master/install.ps1" | Invoke-Expression"
 cls
 C:\%HOMEPATH%\spicetify-cli\spicetify.exe
 cls
 GOTO InstallSpicetifyStep2
+::Step2
 :InstallSpicetifyStep2
 echo Que voulez-vous faire ? (Repondez avec le numero) : 
 echo 1 - Installer des themes
 echo 2 - Ne pas installer de themes
-SET /p reponse2=""
+echo.
+SET /p reponse2="Votre choix : "
 If /i "%reponse2%"=="" GOTO DownloadThemes
 If /i "%reponse2%"=="1" GOTO DownloadThemes
 If /i "%reponse2%"=="2" GOTO NoDownloadThemes
@@ -56,7 +82,8 @@ C:\%HOMEPATH%\spicetify-cli\spicetify.exe config current_theme %whichtheme%
 C:\%HOMEPATH%\spicetify-cli\spicetify.exe backup
 C:\%HOMEPATH%\spicetify-cli\spicetify.exe apply
 C:\%HOMEPATH%\spicetify-cli\spicetify.exe update
-SET /p=Appuyez sur la touche Enter pour retourner au menu...
+echo Appuyez sur une touche pour retourner au menu.
+pause > nul
 cls
 GOTO menu
 
@@ -70,7 +97,8 @@ C:\%HOMEPATH%\spicetify-cli\spicetify.exe backup
 C:\%HOMEPATH%\spicetify-cli\spicetify.exe apply
 C:\%HOMEPATH%\spicetify-cli\spicetify.exe update
 cls
-SET /p=Appuyez sur la touche Enter pour retourner au menu...
+echo Appuyez sur une touche pour retourner au menu.
+pause > nul
 cls
 GOTO menu
 
@@ -88,7 +116,8 @@ cls
 C:\%HOMEPATH%\spicetify-cli\spicetify.exe backup
 C:\%HOMEPATH%\spicetify-cli\spicetify.exe apply
 cls
-SET /p=Appuyez sur la touche Enter pour retourner au menu...
+echo Appuyez sur une touche pour retourner au menu.
+pause > nul
 cls
 GOTO menu
 
@@ -97,7 +126,8 @@ GOTO menu
 cls
 C:\%HOMEPATH%\spicetify-cli\spicetify.exe restore backup
 cls
-SET /p=Appuyez sur la touche Enter pour retourner au menu...
+echo Appuyez sur une touche pour retourner au menu.
+pause > nul
 cls
 GOTO menu
 
@@ -107,7 +137,8 @@ cls
 echo Que voulez-vous faire ? (Repondez avec le numero) : 
 echo 1 - Activer le DevTool
 echo 2 - Desactiver le DevTool
-SET /p reponse3=""
+echo.
+SET /p reponse3="Votre choix : "
 If /i "%reponse3%"=="" GOTO EnableDevTool
 If /i "%reponse3%"=="1" GOTO EnableDevTool
 If /i "%reponse3%"=="2" GOTO DisableDevTool
@@ -119,7 +150,8 @@ GOTO :error3
 cls
 C:\%HOMEPATH%\spicetify-cli\spicetify.exe enable-devtool
 cls
-SET /p=Appuyez sur la touche Enter pour retourner au menu...
+echo Appuyez sur une touche pour retourner au menu.
+pause > nul
 cls
 GOTO menu
 
@@ -128,25 +160,42 @@ GOTO menu
 cls
 C:\%HOMEPATH%\spicetify-cli\spicetify.exe disable-devtool
 cls
-SET /p=Appuyez sur la touche Enter pour retourner au menu...
+echo Appuyez sur une touche pour retourner au menu.
+pause > nul
 cls
 GOTO menu
+
+::Menu bloqueur de pubs
+:DisableAdsMenu
+cls
+%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -command "Start-BitsTransfer -Source https://raw.githubusercontent.com/Zalatis/Install-spicetify-cli/master/AdsManagerFR.bat -Destination C:\%HOMEPATH%\Downloads\AdsManager.bat"
+call C:\%HOMEPATH%\Downloads\AdsManagerFR.bat
+cls
+echo *************************************************
+echo *         Execution de AdsManager...            *
+echo *************************************************
+echo Appuyez sur une touche pour retourner au menu.
+pause > nul
+goto menu
 
 ::Erreur
 :error1
-SET /p=Appuyez sur la touche Enter pour retourner au menu...
+echo Appuyez sur une touche pour retourner au menu.
+pause > nul
 cls
-GOTO menu
+goto menu
 
 ::Erreur
 :error2
-SET /p=Appuyez sur la touche Enter pour retourner au menu...
+echo Appuyez sur une touche pour retourner au menu.
+pause > nul
 cls
 GOTO InstallSpicetifyStep2
 
 ::Erreur
 :error3
-SET /p=Appuyez sur la touche Enter pour retourner au menu...
+echo Appuyez sur une touche pour retourner au menu.
+pause > nul
 cls
 GOTO DevTool
 
